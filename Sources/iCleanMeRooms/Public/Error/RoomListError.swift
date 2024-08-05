469af52667c68039a7bc6352e423df86f6a525c0
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2024-08-05 15:49:51 -0700

    commented out DisplayableError conformance

--- Sources/iCleanMeRooms/Public/Error/RoomListError.swift
+++ Sources/iCleanMeRooms/Public/Error/RoomListError.swift
@@ -5,7 +5,7 @@
 //  Created by Nikolai Nobadi on 7/9/24.
 //
 
-import iCleanMeSharedUI
+//import iCleanMeSharedUI
 
 public enum RoomListError: Error {
     case roomLimitReached
@@ -22,45 +22,45 @@ public enum RoomListError: Error {
 
 
 // MARK: - DisplayableError
-extension RoomListError: DisplayableError {
-    public var message: String {
-        switch self {
-        case .roomLimitReached:
-            return "\(addRoomErrorPrefix) upgrade to iCleanMe Pro \(addRoomErrorSuffix)"
-        case .guestLimitReached:
-            return "\(addRoomErrorPrefix) create an account by adding a sign-in method (Settings > Profile) \(addRoomErrorSuffix)"
-        case .nonAdminEdit:
-            return "Only house admins are allowed to edit room names."
-        case .nonAdminDelete:
-            return "Only house admins are allowed to delete rooms."
-        case .nameTaken(let name):
-            return "There's already a room named \(name). Please select another name."
-        case .emptyName:
-            return "Plants need sunlight, fish need water, and rooms need names. You didn't enter a name for the room. Please do so next time."
-        case .nameTooLong:
-            return "Apologies, but that name is too long for this tiny screen. Please choose a shorter name for your room."
-        case .cancelDelete:
-            return "Your room will NOT be deleted"
-        case .missingRoom:
-            return "Apologies, but the room you want to edit no longer exists, or something terrible has happened to the data. Please try again."
-        case .corruptData:
-            return "Something horrible went wrong with your data. If you continue experiencing problems, please send me feedback (which you can do from Settings)"
-        }
-    }
-}
-
-
-// MARK: - Private Helpers
-private extension RoomListError {
-    private var addRoomErrorSuffix: String {
-        "or delete another room to make space."
-    }
-    
-    private var addRoomErrorPrefix: String {
-        """
-        You've reached your house room limit.
-        
-        In order to add more rooms, please
-        """
-    }
-}
+//extension RoomListError: DisplayableError {
+//    public var message: String {
+//        switch self {
+//        case .roomLimitReached:
+//            return "\(addRoomErrorPrefix) upgrade to iCleanMe Pro \(addRoomErrorSuffix)"
+//        case .guestLimitReached:
+//            return "\(addRoomErrorPrefix) create an account by adding a sign-in method (Settings > Profile) \(addRoomErrorSuffix)"
+//        case .nonAdminEdit:
+//            return "Only house admins are allowed to edit room names."
+//        case .nonAdminDelete:
+//            return "Only house admins are allowed to delete rooms."
+//        case .nameTaken(let name):
+//            return "There's already a room named \(name). Please select another name."
+//        case .emptyName:
+//            return "Plants need sunlight, fish need water, and rooms need names. You didn't enter a name for the room. Please do so next time."
+//        case .nameTooLong:
+//            return "Apologies, but that name is too long for this tiny screen. Please choose a shorter name for your room."
+//        case .cancelDelete:
+//            return "Your room will NOT be deleted"
+//        case .missingRoom:
+//            return "Apologies, but the room you want to edit no longer exists, or something terrible has happened to the data. Please try again."
+//        case .corruptData:
+//            return "Something horrible went wrong with your data. If you continue experiencing problems, please send me feedback (which you can do from Settings)"
+//        }
+//    }
+//}
+//
+//
+//// MARK: - Private Helpers
+//private extension RoomListError {
+//    private var addRoomErrorSuffix: String {
+//        "or delete another room to make space."
+//    }
+//    
+//    private var addRoomErrorPrefix: String {
+//        """
+//        You've reached your house room limit.
+//        
+//        In order to add more rooms, please
+//        """
+//    }
+//}
