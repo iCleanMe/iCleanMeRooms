2d9963a71dd14786a8eeb2d6cffa87f640d436ec
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2024-08-09 12:13:21 -0700

    added new PanGestureViewModifier (should move to NnSwiftUIKit)

--- Sources/iCleanMeRooms/Internal/UI/List/RoomListView.swift
+++ Sources/iCleanMeRooms/Internal/UI/List/RoomListView.swift
@@ -13,20 +13,6 @@ struct RoomListView: View {
     @State private var showingAddButton = true
     @StateObject var viewModel: RoomListViewModel
     
-    func handleGesture(_ gesture: UIPanGestureRecognizer) {
-        let velocityY = gesture.velocity(in: gesture.view).y
-        
-        if velocityY < 0 {
-            if -(velocityY / 5) > 60 && showingAddButton {
-                showingAddButton = false
-            }
-        } else {
-            if (velocityY / 5) > 40 && !showingAddButton {
-                showingAddButton = true
-            }
-        }
-    }
-    
     var body: some View {
         VStack {
             PlainRoomListView {
@@ -45,11 +31,9 @@ struct RoomListView: View {
             PlainRoomListView(leadingInsetPercent: 1) {
                 RoomListSection(section: viewModel.sectionToDisplay, navHandler: viewModel)
             }
-            .background(
-                CustomGesture {
-                    handleGesture($0)
-                }
-            )
+            .handlingVerticalPanGesture {
+                showingAddButton = $0 == .down
+            }
             .setRoomListIdAccessId(.roomSectionList)
             .showingConditionalView(isShowing: viewModel.showNonProPersonalList) {
                 NonProPersonalRoomsListView()
@@ -183,50 +167,5 @@ fileprivate struct AddButton: View {
         .withGradientBackground(.seaNight)
         .clipShape(Circle())
         .shadow(color: .primary, radius: 7)
-    }
-}
-
-
-struct CustomGesture: UIViewRepresentable {
-    private let gestureId = UUID().uuidString
-    
-    var onChange: (UIPanGestureRecognizer) -> Void
-    
-    func makeCoordinator() -> Coordinator {
-        return .init(onChange: onChange)
-    }
-    
-    func makeUIView(context: Context) -> some UIView {
-        return .init()
-    }
-    
-    func updateUIView(_ uiView: UIViewType, context: Context) {
-        DispatchQueue.main.async {
-            if let superview = uiView.superview?.superview, !(superview.gestureRecognizers?.contains(where: { $0.name == gestureId }) ?? false) {
-                let gesture = UIPanGestureRecognizer(target: context.coordinator, action: #selector(context.coordinator.gestureChange(gesture:)))
-                
-                gesture.name = gestureId
-                gesture.delegate = context.coordinator
-                
-                superview.addGestureRecognizer(gesture)
-            }
-        }
-    }
-    
-    class Coordinator: NSObject, UIGestureRecognizerDelegate {
-        var onChange: (UIPanGestureRecognizer) -> Void
-        
-        init(onChange: @escaping (UIPanGestureRecognizer) -> Void) {
-            self.onChange = onChange
-        }
-        
-        @objc
-        func gestureChange(gesture: UIPanGestureRecognizer) {
-            onChange(gesture)
-        }
-        
-        func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
-            return true
-        }
     }
 }
